f8a6a21a3ac98fa9916dc70ab9eb74d5e8d248dc
majd <majd1999ziad@gmail.com>
2019-08-12 17:07:35 +0300

    added "FileParsingFactory" interface with method "fileParseFactory"

---
+++ FileParsing/src/fileparsing/FileParsingFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileparsing;
+
+/**
+ *
+ * @author majd1
+ */
+public interface FileParsingFactory {
+    public FileParser fileParserFactory(String fileName);
+    
+}
