3f0fdb94876b22654420b99b1eaaea02fc681936
majd <majd1999ziad@gmail.com>
2019-08-13 13:44:21 +0300

    added a method to get "xml string " from child npdes in xml document

--- FileParsing/src/fileparsing/BasicClasses/XmlFileParser.java
+++ FileParsing/src/fileparsing/BasicClasses/XmlFileParser.java
@@ -5,6 +5,9 @@
  */
 package fileparsing.BasicClasses;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author majd1
@@ -14,6 +17,30 @@ public class XmlFileParser extends FileParser{
     @Override
     public void parseFile(String FileName) {
         
+    }
+    
+    
+    private static String printChildNodes (NodeList nodeList){
+       String xmlString="";
+       for(int i=0;i<nodeList.getLength();i++){
+            Node node = nodeList.item(i);
+            // get  subNodes
+            if (node.hasChildNodes())
+                for (int count =0 ; count <node.getChildNodes().getLength(); count++){
+                    Node tempNode = node.getChildNodes().item(count);
+                    // make sure it's element node.
+                    if(tempNode.getNodeType()==Node.ELEMENT_NODE){
+                        // get subnode name and value
+		        xmlString+=tempNode.getNodeName().substring(0, 1).toUpperCase()+tempNode.getNodeName().substring(1).toLowerCase();
+                        xmlString+=":"+tempNode.getTextContent().substring(0, 1).toUpperCase()+tempNode.getTextContent().substring(1).toLowerCase();
+                        xmlString+="\n";
+		        xmlString+=printChildNodes(tempNode.getChildNodes());
+                    }
+                    if(tempNode.hasChildNodes())
+                        xmlString+=printChildNodes(tempNode.getChildNodes());
+            }
+        }
+        return  xmlString;
     }
     
 }
